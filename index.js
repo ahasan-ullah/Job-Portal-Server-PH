fee85c032e6c9cac2314904c978fdd5d87463588
ahasan-ullah <ahasanullah451@gmail.com>
2024-12-19 00:43:21 +0600

    job application api added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
@@ -33,12 +33,37 @@ async function run() {
 
     // jobs related api
     const jobsCollection = client.db("jobPortal").collection("jobs");
+    const jobApplicationCollection=client.db('jobPortal').collection('job_applications')
 
     app.get("/jobs", async (req, res) => {
       const cursor=jobsCollection.find();
       const result=await cursor.toArray();
       res.send(result);
     });
+
+    app.get('/jobs/:id',async(req,res)=>{
+      const id=req.params;
+      const query={_id: new ObjectId(id)};
+      const result= await jobsCollection.findOne(query);
+      res.send(result);
+    })
+
+
+
+
+    //job application api
+    app.post('/job_application',async(req,res)=>{
+      const application=req.body;
+      const result=await jobApplicationCollection.insertOne(application);
+      res.send(result);
+    })
+
+
+
+
+
+
+
 
     await client.db("admin").command({ ping: 1 });
     console.log(
