4c4cd596b453b81624e4f449b1a3b30ebb5bd575
ahasan-ullah <ahasanullah451@gmail.com>
2024-12-20 00:18:22 +0600

    parameterized api added for application job table

--- index.js
+++ index.js
@@ -62,6 +62,19 @@ async function run() {
       const email=req.query.email;
       const query={applicant_email: email}
       const result=await jobApplicationCollection.find(query).toArray();
+
+      // bad way
+      for(const application of result){
+        const query1={_id: new ObjectId(application.job_id)};
+        const job=await jobsCollection.findOne(query1);
+        if(job){
+          application.title=job.title;
+          application.company=job.company;
+          application.location=job.location;
+          application.company_logo=job.company_logo;
+        }
+      }
+
       res.send(result);
     })
 
